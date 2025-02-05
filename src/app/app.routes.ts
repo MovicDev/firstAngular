86e9d219566d411dbc2814d1e46a4ba20b3855e4
ibrahimkolawole <ibrahimkolawole564@gmail.com>
2025-02-05 14:19:07 +0100

    updated class Work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,17 @@ import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+import { UsersComponent } from './Components/user/users/users.component';
+import { ProfileComponent } from './Components/profile/profile/profile.component';
 
 export const routes: Routes = [
     {path : '', component : HomeComponent, title: 'Home' },
     {path : 'Signup', component : SignupComponent, title: 'SignUp' },
     {path : 'Login', component : LoginComponent, title: 'Login' },
-    {path : '**', component : PageNotFoundComponent, title: '404|Page Not Found' }
+    {path: 'home', redirectTo: '', pathMatch: 'full'},
+    {path: 'users', children: [
+        {path:'', component: UsersComponent},
+        {path:'profile', component: ProfileComponent}
+    ]},
+    {path : '**', component : PageNotFoundComponent, title: '404|Page Not Found' },
 ];
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {FormsModule, FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule,CommonModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
+  standalone: true,
 })
 export class SignupComponent {
 
+  // userForm:FormGroup; //Creating new instance of the Formgroup class 
+
+      // constructor(
+      //   private _fb: FormBuilder,
+      //   private _formGroup: FormGroup
+      // ){
+      //  this.userForm = this._fb.group({
+      //     'full_name' : ['', [Validators.required]],
+      //     'email':['', [Validators.required,Validators.email]],
+      //     'phone_number': ['', [Validators.required, Validators.minLength(11)]],
+      //     'password': ['', [Validators.required,Validators.minLength(8)]]
+      //   })
+      // }
+
+      private fb = inject(FormBuilder)
+
+      userForm = this.fb.group({
+            'full_name' : ['', [Validators.required]],
+            'email':['', [Validators.required,Validators.email]],
+            'phone_number': ['', [Validators.required, Validators.minLength(11)]],
+            'password': ['', [Validators.required,Validators.minLength(8)]]
+          })
+
+    full_name = '';
+    email = '';
+    phone_number = '';
+    password = '';
+
+    // ngOnInit (){
+    //   this.users=JSON.parse(localStorage['users']); 
+    // }
+    users:any = [];
+
+    register(){
+      // const user = {full_name: this.full_name, email: this.email, phone_number: this.phone_number, password: this.password}     
+      // console.log(user);
+      // this.users.push(user);
+      // localStorage.setItem('users',JSON.stringify(this.users));
+      // console.log("user saved");
+
+      if (this.userForm.invalid) {
+        console.log(this.userForm.get('full_nmae')?.errors);
+        
+      } else {
+        console.log('Form is valid');
+        
+      }
+      
+    }
 }
